7d3b3e71c8dfc3d170c445d995626f7188227c00
Administrator <312967328@qq.com>
2016-10-19 11:52:31 +0800

    init

--- app/Http/Controllers/Admin/IndexController.php
+++ app/Http/Controllers/Admin/IndexController.php
@@ -17,26 +17,9 @@ class IndexController extends Controller
         /*后台菜单配置*/
         $menu = array(
             array('url'=>'admin/index','class'=>'am-icon-home','name'=>'首页'),
-            array('class'=>'am-icon-user-md','name'=>'用户','children'=>array(
-                'admin/user/index'=>array('class'=>'am-icon-users','name'=>'用户管理'),
-            )
-            ),
-            array('class'=>'am-icon-money','name'=>'账户','children'=>array(
-                'admin/getmoney/index'=>array('class'=>'am-icon-rmb','name'=>'提现管理'),
-                'admin/flow/index'=>array('class'=>'am-icon-rmb','name'=>'现金流水'),
-                'admin/flowgold/index'=>array('class'=>'am-icon-usd','name'=>'金币流水'),
-            )
-            ),
             array('class'=>'am-icon-comment','name'=>'信息','children'=>array(
                 'admin/thread/index'=>array('class'=>'am-icon-comments','name'=>'信息管理'),
-                'admin/thread/appointment'=>array('class'=>'am-icon-comments','name'=>'预约信息'),
                 'admin/cate/index'=>array('class'=>'am-icon-twitch','name'=>'分类管理'),
-            )
-            ),
-            array('class'=>'am-icon-shopping-cart','name'=>'商城','children'=>array(
-                'admin/goods/index'=>array('class'=>'am-icon-comments','name'=>'商品管理'),
-                'admin/order/index'=>array('class'=>'am-icon-twitch','name'=>'订单管理'),
-                'admin/keywords/index'=>array('class'=>'am-icon-wordpress','name'=>'关键词管理'),
             )
             ),
             array('class'=>'am-icon-cogs','name'=>'系统','children'=>array(
